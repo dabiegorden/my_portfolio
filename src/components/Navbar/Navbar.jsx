9e2cdb245b6921d880044966a6c896644b57b434
dabiegorden <117079883+dabiegorden@users.noreply.github.com>
2023-07-15 12:23:36 -0700

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,9 @@
 import React from 'react';
 import './Navbar.css';
-import { FaCuttlefish, FaInstagram, FaGithub, FaLinkedin, FaAlignRight } from 'react-icons/fa';
-import { FaXmark } from 'react-icons/fa6';
-import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-
   return (
-    <nav>
-        <Link href="/" className="logo"><span>{FaCuttlefish} Portfolio</span></Link>
-        <ul className="navar">
-            <li>
-              <Link href="#Projects">Projects</Link>
-              </li>
-            <li>
-              <Link href="#Technologies">Technologies</Link>
-              </li>
-            <li>
-              <Link href="#About">About</Link>
-              </li>
-        </ul>
-        <div className="socials">
-              <FaInstagram />
-              <FaGithub />
-              <FaLinkedin />
-        </div>
-    </nav>
+    <div>Navbar</div>
   )
 }
 
