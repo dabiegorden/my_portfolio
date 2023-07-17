7aca5cd57f6b3ff5e2d1eb7167c0a6f6ca4d1f93
dabiegorden <117079883+dabiegorden@users.noreply.github.com>
2023-07-17 15:18:25 -0700

    update

--- src/components/TimeLine/TimeLine.jsx
+++ src/components/TimeLine/TimeLine.jsx
@@ -8,6 +8,10 @@ const TimeLine = () => {
             <h1>About Me</h1>
             <p>The purpose of this portfolio is to help showcase my skills<br/> to customers and potential clients and to work for big<br/> companies and any government work shop as well.</p>
         </div>
+
+        <div className="caurosel__container">
+            <h1>hell</h1>
+        </div>
     </div>
   )
 }
